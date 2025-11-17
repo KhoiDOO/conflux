e0ce47592fd8489b53fe60c830f1cf65f46a2456
KhoiDOO <khoido8899@gmail.com>
2025-11-17 00:24:07 +0000

    example: add argparse CLI for --save_dir, --conf, and --year; use args when invoking crawler.

--- example/crawl_specific.py
+++ example/crawl_specific.py
@@ -1,4 +1,5 @@
 import os, sys
+import argparse
 
 from conflux import CvF_Crawler_Interface
 
@@ -7,8 +8,15 @@ if __name__ == "__main__":
     
     cvf_interface = CvF_Crawler_Interface()
     
+    parser = argparse.ArgumentParser(description="Crawl CVF papers")
+    parser.add_argument("--save_dir", type=str, default=os.getcwd() + "/clone", help="Directory to save papers")
+    parser.add_argument("--conf", type=str, default="cvpr", help="Conference name")
+    parser.add_argument("--year", type=str, nargs='+', default=["2025"], help="Conference year(s)")
+    
+    args = parser.parse_args()
+    
     cvf_interface(
-        save_dir=os.getcwd() + "/clone",
-        conf='cvpr',
-        year=["2022", "2023"]
+        save_dir=args.save_dir,
+        conf=args.conf,
+        year=args.year
     )
