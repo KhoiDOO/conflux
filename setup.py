6faac0c92a2843b2bf6d71baaccf459be52aa669
KhoiDOO <khoido8899@gmail.com>
2025-11-18 16:24:42 +0000

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="conflux",
-    version="0.0.5",
+    version="0.0.6",
     description="A simple crawler to download papers from major computer vision conferences",
     long_description=long_description,
     long_description_content_type="text/markdown",
